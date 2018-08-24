48f06dcb0e86fe9cb6e2177940918128f64a6dd1
eos <978663747@qq.com>
2018-08-24 15:46:12 +0800

    修改备份数据操作

--- eosio-node/get_ubuntu_data.sh
+++ eosio-node/get_ubuntu_data.sh
@@ -1,6 +1,9 @@
+cd eos-data/
 
 wget $1 
 
 ls > data-name
+
 echo "data-name : $data-name"
-tar zxvf $data-name ./eos-data 
+
+tar zxvf $data-name 
--- eosio-node/set_eosioservice.sh
+++ eosio-node/set_eosioservice.sh
@@ -2,5 +2,4 @@ mkdir -p /usr/lib/systemd/system/
 
 cp ./eosio.service /usr/lib/systemd/system/
 
-systemctl start eosio.service
 
